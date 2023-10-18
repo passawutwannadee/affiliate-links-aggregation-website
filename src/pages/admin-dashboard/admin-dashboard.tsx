63b4c86d92d3e644ec4c5fd2a6d4af620975a868
Passawut Wannadee <passawut@tuta.io>
2023-10-18 23:42:23 +0700

    working on admin dashboard

--- src/pages/admin-dashboard/admin-dashboard.tsx
+++ src/pages/admin-dashboard/admin-dashboard.tsx
@@ -38,108 +38,79 @@ import {
   TableRow,
 } from '@/components/ui/table';
 
-const data: Payment[] = [
+const data: Report[] = [
   {
-    id: 'm5gr84i9',
-    amount: 316,
-    status: 'success',
-    email: 'ken99@yahoo.com',
-  },
-  {
-    id: '3u1reuv4',
-    amount: 242,
-    status: 'success',
-    email: 'Abe45@gmail.com',
-  },
-  {
-    id: 'derv1ws0',
-    amount: 837,
-    status: 'processing',
-    email: 'Monserrat44@gmail.com',
-  },
-  {
-    id: '5kma53ae',
-    amount: 874,
-    status: 'success',
-    email: 'Silas22@gmail.com',
-  },
-  {
-    id: 'bhqecj4p',
-    amount: 721,
-    status: 'failed',
-    email: 'carmella@hotmail.com',
+    reported_user: 'John Doe',
+    report_category: 'Illegal Item',
+    report_status: 'processing',
+    report_detail:
+      'This guy pretends to sell fry chicken, but he actually is selling crystal meth.',
   },
 ];
 
-export type Payment = {
-  id: string;
-  amount: number;
-  status: 'pending' | 'processing' | 'success' | 'failed';
-  email: string;
+export type Report = {
+  reported_user: string;
+  report_category: string;
+  report_status: 'processing' | 'closed';
+  report_detail: string;
 };
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<Report>[] = [
+  // {
+  //   id: 'select',
+  //   // header: ({ table }) => (
+  //   // //   <Checkbox
+  //   // //     checked={table.getIsAllPageRowsSelected()}
+  //   // //     onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+  //   // //     aria-label="Select all"
+  //   // //   />
+  //   // ),
+  //   // cell: ({ row }) => (
+  //   // //   <Checkbox
+  //   // //     checked={row.getIsSelected()}
+  //   // //     onCheckedChange={(value) => row.toggleSelected(!!value)}
+  //   // //     aria-label="Select row"
+  //   // //   />
+  //   // ),
+  //   enableSorting: false,
+  //   enableHiding: false,
+  // },
   {
-    id: 'select',
-    // header: ({ table }) => (
-    // //   <Checkbox
-    // //     checked={table.getIsAllPageRowsSelected()}
-    // //     onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-    // //     aria-label="Select all"
-    // //   />
-    // ),
-    // cell: ({ row }) => (
-    // //   <Checkbox
-    // //     checked={row.getIsSelected()}
-    // //     onCheckedChange={(value) => row.toggleSelected(!!value)}
-    // //     aria-label="Select row"
-    // //   />
-    // ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: 'status',
-    header: 'Status',
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue('status')}</div>
-    ),
-  },
-  {
-    accessorKey: 'email',
+    accessorKey: 'reported_user',
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
-          Email
+          Reported User
           <CaretSortIcon className="ml-2 h-4 w-4" />
         </Button>
       );
     },
-    cell: ({ row }) => <div className="lowercase">{row.getValue('email')}</div>,
+    cell: ({ row }) => (
+      <div className="lowercase">{row.getValue('reported_user')}</div>
+    ),
   },
   {
-    accessorKey: 'amount',
-    header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue('amount'));
-
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      }).format(amount);
-
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
+    accessorKey: 'report_category',
+    header: () => <div className="text-left">Reported Category</div>,
+    cell: ({ row }) => (
+      <div className="lowercase">{row.getValue('report_category')}</div>
+    ),
+  },
+  {
+    accessorKey: 'report_status',
+    header: 'Status',
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue('report_status')}</div>
+    ),
   },
   {
     id: 'actions',
     enableHiding: false,
     cell: ({ row }) => {
-      const payment = row.original;
+      // const Report = row.original;
 
       return (
         <DropdownMenu>
@@ -151,14 +122,14 @@ export const columns: ColumnDef<Payment>[] = [
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
+            {/* <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(Report.id)}
             >
-              Copy payment ID
-            </DropdownMenuItem>
+              Copy Report ID
+            </DropdownMenuItem> */}
             <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
+            <DropdownMenuItem>View detail</DropdownMenuItem>
+            <DropdownMenuItem>Close Ticket</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       );
