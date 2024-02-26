50a42bae131550d66085cc3ca4bda3c200cb312f
Passawut Wannadee <passawut@tuta.io>
2024-02-26 23:38:16 +0700

    fix: wording

--- src/pages/admin-dashboard/components/data-table-toolbar.tsx
+++ src/pages/admin-dashboard/components/data-table-toolbar.tsx
@@ -53,7 +53,7 @@ export function DataTableToolbar<TData>({
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <Input
-          placeholder="Filter tasks..."
+          placeholder="Filter username"
           value={
             (table.getColumn('username')?.getFilterValue() as string) ?? ''
           }
